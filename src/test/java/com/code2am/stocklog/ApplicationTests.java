33f16dd7b0de423c1c1044ad9623135cbade3965
isac7722 <isac7722@gmail.com>
2024-02-21 13:02:46 +0900

    first commit

---
+++ src/test/java/com/code2am/stocklog/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code2am.stocklog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
