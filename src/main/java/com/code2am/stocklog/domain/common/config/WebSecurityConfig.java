87510fe427c12c836c9139ecc9c447c09f512123
hiimparkjiho <jiho2689@gmail.com>
2024-02-22 13:36:40 +0900

    fetch 카카오 로그인 기능(미완)

--- src/main/java/com/code2am/stocklog/domain/common/config/WebSecurityConfig.java
+++ src/main/java/com/code2am/stocklog/domain/common/config/WebSecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration  // 환경 구성?
@@ -18,21 +21,23 @@ public class WebSecurityConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // Cross-Site Request Forgery
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> { // 서버의 리소스에 접근 가능한 권한을 설정함
-                    //여기부터 로그인 권한을 설정하는 공간
-                    auth.requestMatchers("/**").permitAll();    // url 기반으로 접근 허용
+                .authorizeHttpRequests(auth -> {
+                    // URL 기반으로 접근 허용
+                    auth.requestMatchers("/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
-
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable());
+                // SNS 로그인을 위한 OAuth 2.0 로그인 구성
+                //.oauth2Login(oauth2 -> oauth2.clientRegistrationRepository(clientRegistrationRepository));
+
         // SpringFilterChain에 걸어줌 build
         return http.build();
     }
-
 
 }
