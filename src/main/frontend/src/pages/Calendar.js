f9498632d699d947f05ee841d5005b0d086f032e
ClownYH <qwer1745@gmail.com>
2024-02-25 17:22:38 +0900

    Feat : 연간 캘린더 추가

--- src/main/frontend/src/pages/Calendar.js
+++ src/main/frontend/src/pages/Calendar.js
@@ -1,8 +1,37 @@
+import FullCalendar from "@fullcalendar/react";
+import multiMonthPlugin from "@fullcalendar/multimonth";
+import interactionPlugin from "@fullcalendar/interaction"
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const Calendar = () => {
 
+    const navigate = useNavigate();
+
+    const linkToMonthHandler = (info) => {
+        const month = info.getMonth() + 1; // 특정 월
+        alert(month);
+        navigate("/calendar/" + month);
+    }
+
+
     return (
         <>
+            <FullCalendar
+                plugins={[multiMonthPlugin, interactionPlugin]}
+                initialView="multiMonthYear"
+                locale="kr"
+                headerToolbar={{
+                    center:"title",
+                    start:"prev",
+                    end:"next"
+                }}
+                footerToolbar={{
+                    center:"today"
+                }}
+                navLinks={true}
+                navLinkDayClick={linkToMonthHandler}
+            />
         </>
     )
 }
