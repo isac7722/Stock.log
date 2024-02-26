ef6b56e257592d411564201838014e2b2e7bbda0
ClownYH <qwer1745@gmail.com>
2024-02-26 14:42:04 +0900

    css : 내비게이션바 css 1차적으로 추가

--- src/main/frontend/src/components/NavBar.js
+++ src/main/frontend/src/components/NavBar.js
@@ -3,18 +3,18 @@ import {Link} from "react-router-dom";
 const NavBar = () => {
 
     return(
-        <nav>
-            <div>
-                <img src={"/img/logo.png"} alt={"로고 이미지"}/>
-            </div>
-            <div>
-                <h6>MAIN MENU</h6>
-                <Link to={"/main"}>메인화면</Link>
-                <Link to={"/journals"}>매매일지</Link>
-                <Link to={"/calendar"}>캘린더</Link>
-                <Link to={"/trader"}>트레이더스</Link>
-                <Link to={"/settings"}>설정</Link>
-                <button type={"button"}>Logout</button>
+        <nav className={"navbar"}>
+            <imageWrapper id={"logoBox"} >
+                <img id={"logo"} src={"/img/logo.png"} alt={"로고 이미지"}/>
+            </imageWrapper>
+            <div className={"navbarList"}>
+                <h6 id={"navbarTitle"}>MAIN MENU</h6>
+                <Link className={"navbarMenu"} to={"/main"}>메인화면</Link>
+                <Link className={"navbarMenu"} to={"/journals"}>매매일지</Link>
+                <Link className={"navbarMenu"} to={"/calendar"}>캘린더</Link>
+                <Link className={"navbarMenu"} to={"/trader"}>트레이더스</Link>
+                <Link className={"navbarMenu"} to={"/settings"}>설정</Link>
+                <button className={"navbarMenu"} type={"button"}>Logout</button>
             </div>
 
         </nav>
--- src/main/frontend/src/layouts/Layout.js
+++ src/main/frontend/src/layouts/Layout.js
@@ -2,6 +2,7 @@ import {Outlet} from "react-router-dom";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import "../css/Layout.css"
 
 const Layout = () => {
 
