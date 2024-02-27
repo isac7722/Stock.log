02163a917269e4fa05329a838a49bca0cdeae08e
ClownYH <qwer1745@gmail.com>
2024-02-27 09:50:03 +0900

    Fix : 매매전략 기능 구현 내의 userId 획득 방식을 통괄적으로 수정

--- src/main/java/com/code2am/stocklog/domain/auth/common/util/SecurityUtil.java
+++ src/main/java/com/code2am/stocklog/domain/auth/common/util/SecurityUtil.java
@@ -3,8 +3,10 @@ package com.code2am.stocklog.domain.auth.common.util;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class SecurityUtil {
 
     private SecurityUtil() { }
