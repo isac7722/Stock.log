cf586ff5281745cfb4af53699c04d97e8431e44a
ClownYH <qwer1745@gmail.com>
2024-02-27 11:33:21 +0900

    Feat : 매매일지 화면 구성 시작

--- src/main/frontend/src/pages/Journals.js
+++ src/main/frontend/src/pages/Journals.js
@@ -1,8 +1,32 @@
+import JournalSearcher from "../components/journals/JournalSearcher";
 
 const Journals = () => {
 
+    // 테이블 컬럼
+    const tableHeads = ["종목코드", "종목명", "최종거래일", "매수가격", "매수물량", "매도가격", "매도물량", "매매전략", "P&L", ""];
+    const thMap = tableHeads.map(th => {
+        return (
+            <th>{th}</th>
+        )
+    });
+
     return (
         <>
+            <div className={"journalSearcher"}>
+                <JournalSearcher/>
+            </div>
+            <div>
+                <table className={"journalsListTable"}>
+                    <thead>
+                    <tr>
+                        {thMap}
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
