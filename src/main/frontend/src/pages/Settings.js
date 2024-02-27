991b1b624691db174b462a05153b93fe4a95af8d
hiimparkjiho <jiho2689@gmail.com>
2024-02-27 15:28:07 +0900

    featch setting front 진행중

--- src/main/frontend/src/pages/Settings.js
+++ src/main/frontend/src/pages/Settings.js
@@ -1,9 +1,20 @@
+import SettingButton from "../components/setting/SettingButton";
+import SettingInput from "../components/setting/SettingInput";
 
 const Settings = () => {
 
     return (
-        <>
-        </>
+        <div className="settingDiv">
+            <SettingInput
+                placeholder={"email"}
+           />
+            <SettingInput
+                placeholder={"비밀번호"}
+           />
+           <SettingButton
+                value={"비밀번호 변경"}
+           />
+        </div>
     )
 }
 export default Settings;
