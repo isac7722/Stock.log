8a097ca4001f72024f4e62bdc399e9e7ce8e1203
ClownYH <qwer1745@gmail.com>
2024-02-27 19:10:01 +0900

    Feat : 매매일지 페이지 조회 기능 전체 틀 구성, Api  요청 추가 및 수정이 필요

--- src/main/frontend/src/apis/JournalsAPI.js
+++ src/main/frontend/src/apis/JournalsAPI.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 
 
+// 매매일지 전부 가져오는 요청
 export const callJournalList = async () => {
-    await axios.get("/journals/search/");
+    await axios.get("/journals/search/")
+        .then(data => {
+        if(!data){
+            return null;
+        }
+        return data
+        })
+        .catch(function (error){
+            console.error(error)
+        });
 }
 
+// 백엔드 작업 필요!!
 export async function callJournalListBySearch({text}){
-    await axios.get("/journals/search/");
-};
+    await axios("/journals", {
+        params: {
+            text : text // 검색할 내용
+        }
+    })
+}
--- src/main/frontend/src/pages/Journals.js
+++ src/main/frontend/src/pages/Journals.js
@@ -2,11 +2,14 @@ import JournalSearcher from "../components/journals/JournalSearcher";
 import Table from "react-bootstrap/Table";
 import "../css/journals/journals.css"
 import {useState} from "react";
-import JournalDetail from "../components/journals/journalDetail";
+import {callJournalList} from "../apis/JournalsAPI";
+import JournalList from "../components/journals/JournalList";
 
 const Journals = () => {
 
-    const [journalList, setJournalList] = useState([{}]);
+    const [journalList, setJournalList] = useState({});
+
+    // 전체 테이블
 
     // 테이블 컬럼
     const tableHeads = ["종목코드", "종목명", "최종거래일", "매수가격", "매수물량", "매도가격", "매도물량", "매매전략", "P&L", "수정"];
@@ -16,10 +19,12 @@ const Journals = () => {
         )
     });
 
+    setJournalList(callJournalList());
+
     return (
         <>
             <div className={"journalSearcher"}>
-                <JournalSearcher journalList={journalList} setJournalList={setJournalList}/>
+                {/*<JournalSearcher journalList={journalList} setJournalList={setJournalList}/>*/}
             </div>
             <div>
                 <Table bordered hover>
@@ -29,7 +34,7 @@ const Journals = () => {
                     </tr>
                     </thead>
                     <tbody>
-                        <JournalDetail journalList={journalList}/>
+                        <JournalList journalList={journalList}/>
                     </tbody>
                 </Table>
             </div>
