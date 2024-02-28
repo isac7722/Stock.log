e79de0c8e6714ce0ce24fff868cff8a9e3783078
hiimparkjiho <jiho2689@gmail.com>
2024-02-29 04:20:54 +0900

    fix: 백엔드에서 주식 종목 DB에 insert(미완)

--- src/main/java/com/code2am/stocklog/domain/stocks/controller/StocksController.java
+++ src/main/java/com/code2am/stocklog/domain/stocks/controller/StocksController.java
@@ -1,26 +1,30 @@
 package com.code2am.stocklog.domain.stocks.controller;
 
 
+import com.code2am.stocklog.domain.stocks.models.dto.StocksDTO;
 import com.code2am.stocklog.domain.stocks.service.StocksService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.net.http.HttpHeaders;
+import java.util.List;
 
 @RestController
 @RequestMapping("/stocks")
 public class StocksController {
 
-    private final StocksService stocksService;
+        private final StocksService stocksService;
 
-    public StocksController(StocksService stocksService) {
-        this.stocksService = stocksService;
-    }
+        public StocksController(StocksService stocksService) {
+            this.stocksService = stocksService;
+        }
 
-    @GetMapping
-    public void getStocks() throws JsonProcessingException {
-        // 서비스 로직 호출
-        stocksService.saveStocks();
-    }
+        @GetMapping("/data")
+        public Mono<StocksDTO> getExternalData() {
+            return stocksService.fetchDataFromExternalAPI();
+        }
+
 
 }
--- src/main/java/com/code2am/stocklog/domain/stocks/service/StocksService.java
+++ src/main/java/com/code2am/stocklog/domain/stocks/service/StocksService.java
@@ -1,52 +1,45 @@
 package com.code2am.stocklog.domain.stocks.service;
 
+import com.code2am.stocklog.domain.stocks.models.dto.StocksDTO;
 import com.code2am.stocklog.domain.stocks.models.entity.Stocks;
-import com.code2am.stocklog.domain.stocks.repository.StocksRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.Map;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class StocksService {
-    private final String SERVICE_KEY = "Bt%2BhVFL95%2FMEthp5iyplyQGh16yHgcF65moL7eFYRF%2Bqc1rnE8ANo4qPuY%2B2C2qBCQL090e0I%2Bf5GE9IdYs%2BEw%3D%3D";
+    private final WebClient webClient;
+    private final String serviceKey = "Bt%2BhVFL95%2FMEthp5iyplyQGh16yHgcF65moL7eFYRF%2Bqc1rnE8ANo4qPuY%2B2C2qBCQL090e0I%2Bf5GE9IdYs%2BEw%3D%3D";
 
-    @Autowired
-    private  StocksRepository stocksRepository;
+    public StocksService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl("https://apis.data.go.kr/1160100/service/GetKrxListedInfoService/getItemInfo").build();
+    }
 
-    public void saveStocks() throws JsonProcessingException {
-        // API 호출
-        String url = "http://apis.data.go.kr/1160100/service/GetKrxListedInfoService/getItemInfo?format=json&basDt=20240227&serviceKey=" + SERVICE_KEY;
-        String response = "";
+    public Mono<StocksDTO> fetchDataFromExternalAPI() {
+        return webClient.get()
+                .uri("?resultType=json&serviceKey=" + serviceKey)
+                .retrieve()
+                .bodyToMono(String.class) // JSON 문자열로 응답을 받음
+                .map(this::extractDesiredValues); // 원하는 값만 추출하여 StocksDTO 객체로 매핑
+    }
 
+    private StocksDTO extractDesiredValues(String responseBody) {
+        ObjectMapper objectMapper = new ObjectMapper();
         try {
-            // HTTP GET 요청을 통해 API 응답 받기
-            response = new RestTemplate().getForObject(url, String.class);
-        } catch (HttpClientErrorException e) {
-            // API 호출 에러 처리
-            System.out.println("API 호출 에러: " + e.getMessage());
-            return;
-        }
-
-        // 데이터 파싱
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> data = mapper.readValue(response, Map.class);
-
-        List<Map<String, Object>> stockList = (List<Map<String, Object>>) data.get("data");
-
-        // DB에 저장
-        for (Map<String, Object> stockData : stockList) {
-            Stocks stock = new Stocks();
-
-            stock.setStockCode((String) stockData.get("isinCode"));
-            stock.setStockName((String) stockData.get("stockName"));
-
-            stocksRepository.save(stock);
+            JsonNode rootNode = objectMapper.readTree(responseBody);
+            // JSON 노드에서 필요한 값을 추출하여 새로운 DTO 객체를 생성
+            String stockName = rootNode.path("itmsNm").asText(); // stockName에 해당하는 값 추출
+            String stockCode = rootNode.path("isinCd").asText(); // stockCode에 해당하는 값 추출
+            StocksDTO stocksDTO = new StocksDTO();
+            stocksDTO.setStocksName(stockName); // DTO에 설정
+            stocksDTO.setStocksCode(stockCode); // DTO에 설정
+            return stocksDTO;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
