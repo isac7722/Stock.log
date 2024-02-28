d2c141412fdd89019a696c83f34a51d9c7a07309
hiimparkjiho <jiho2689@gmail.com>
2024-02-28 14:54:16 +0900

    fix : navbar 프론트 작업

--- src/main/frontend/src/components/NavBar.js
+++ src/main/frontend/src/components/NavBar.js
@@ -1,10 +1,16 @@
 import {Link} from "react-router-dom";
+import "../css/Layout.css"
+import Button from 'react-bootstrap/Button';
+
 
 const NavBar = ({pageInfo}) => {
 
     const pageInfoList = () => pageInfo.map(page => {
         return(
+            <div className="navDiv">
+            <img src={page.imgUrl} className="navLogo" />
             <Link className={"navbarMenu"} to={page.url}>{page.name}</Link>
+            </div>
         )
     })
 
@@ -15,9 +21,12 @@ const NavBar = ({pageInfo}) => {
                     <img id={"logo"} src={"/img/logo.png"} alt={"로고 이미지"}/>
                 </div>
                 <div className={"navbarList"}>
+                    <Button size="lg" id="journalsAddBtn">
+                        매매일지 추가
+                    </Button>
                     <h6 id={"navbarTitle"}>MAIN MENU</h6>
                     {pageInfoList()}
-                    <button className={"navbarMenu"} type={"button"}>Logout</button>
+                    {/* <button className={"navbarMenu"} type={"button"}>Logout</button> */}
                 </div>
 
             </nav>
